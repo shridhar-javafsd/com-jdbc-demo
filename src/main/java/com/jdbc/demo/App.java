3c6c4fc75076949f48441cb55bf74b27eb119550
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-07-17 01:50:15 +0530

    Initial commit

---
+++ src/main/java/com/jdbc/demo/App.java
@@ -0,0 +1,20 @@
+package com.jdbc.demo;
+
+/**
+ * 
+ * @author Vaman Deshmukh
+ *
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Start");
+		
+		
+
+		System.out.println("End");
+
+	}
+}
---
+++ src/test/java/com/jdbc/demo/AppTest.java
@@ -0,0 +1,5 @@
+package com.jdbc.demo;
+
+public class AppTest {
+
+}
